87b00351db088f5e4caa82ca3484ec52aebc1ffc
Jim Sugg <jim.sugg@brightsign.biz>
2017-05-29 15:30:31 -1000

    Added more tests for fractional dimensions on all file types

--- test.js
+++ test.js
@@ -45,13 +45,63 @@ test('resize bmp image', async t => {
 	});
 });
 
-test('resize image using only width', async t => {
+test('resize png image using only width', async t => {
 	const data = await fn(await readFile('fixture.png'), {width: 150});
 
 	t.deepEqual(imageSize(data), {
 		width: 150,
 		height: 99,
 		type: 'png'
+	});
+});
+
+test('resize jpg image using only width', async t => {
+	const data = await fn(await readFile('fixture.jpg'), {width: 150});
+
+	t.deepEqual(imageSize(data), {
+		width: 150,
+		height: 99,
+		type: 'jpg'
+	});
+});
+
+test('resize bmp image using only width', async t => {
+	const data = await fn(await readFile('fixture.bmp'), {width: 150});
+
+	t.deepEqual(imageSize(data), {
+		width: 150,
+		height: 99,
+		type: 'bmp'
+	});
+});
+
+test('resize png image using only height', async t => {
+	const data = await fn(await readFile('fixture.png'), {height: 100});
+
+	t.deepEqual(imageSize(data), {
+		width: 150,
+		height: 100,
+		type: 'png'
+	});
+});
+
+test('resize jpg image using only height', async t => {
+	const data = await fn(await readFile('fixture.jpg'), {height: 100});
+
+	t.deepEqual(imageSize(data), {
+		width: 150,
+		height: 100,
+		type: 'jpg'
+	});
+});
+
+test('resize bmp image using only height', async t => {
+	const data = await fn(await readFile('fixture.bmp'), {height: 100});
+
+	t.deepEqual(imageSize(data), {
+		width: 150,
+		height: 100,
+		type: 'bmp'
 	});
 });
 
